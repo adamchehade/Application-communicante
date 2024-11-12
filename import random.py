16973312bbe175f31845e1a9e6a011861c789bda
adamchehade <chehadeadam64@gmail.com>
2024-11-12 18:17:13 +0100

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# Generate a password of 12 characters by default
+password = generate_password()
+print("Generated Password:", password)
